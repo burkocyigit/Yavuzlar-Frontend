4b8864b3da16f46618ec4ef0f332de1f09ecff64
Burak <brkkcygt32@gmail.com>
2024-01-28 21:35:26 +0300

    change lang

--- Week04/app.js
+++ Week04/app.js
@@ -12,14 +12,41 @@ navBtnDOM.addEventListener("click", () => {
   links.classList.toggle("show-nav-content");
 });
 
+const langs = getElement(".langs-opt");
+const langFromOptBtnDOM = getElement("#translateFrom");
+const langToOptBtnDOM = getElement("#translateTo");
+
+langFromOptBtnDOM.addEventListener("click", () => {
+  langs.classList.toggle("show-lang-content");
+});
+
+langToOptBtnDOM.addEventListener("click", () => {
+  langs.classList.toggle("show-lang-content");
+});
+
 const url = "https://api-free.deepl.com/v2/translate";
-const authKey = "";
+const authKey = "1d22810f-fe2b-489c-a446-443ab9e7ca9f:fx";
 const userAgent = "YourApp/1.2.3";
 
 const headers = new Headers({
   Authorization: `DeepL-Auth-Key ${authKey}`,
   "User-Agent": userAgent,
   "Content-Type": "application/json",
+});
+
+const translateFromBtn = document.getElementById("translateFrom");
+translateFromBtn.addEventListener("click", () => {
+  console.log("translatefrombtn clicked");
+});
+
+const swapLangBtn = document.getElementById("swap-lang");
+swapLangBtn.addEventListener("click", () => {
+  console.log("swaplanbtn clicked");
+});
+
+const translateToBtn = document.getElementById("translateTo");
+translateToBtn.addEventListener("click", () => {
+  console.log("translateToBtn clicked");
 });
 
 async function translate(text, targetLang) {
@@ -40,7 +67,7 @@ async function translate(text, targetLang) {
     return data.translations[0].text;
   } catch (error) {
     console.error("Error:", error);
-    throw error; // rethrow the error to be handled by the caller if needed
+    throw error;
   }
 }
 
@@ -65,7 +92,14 @@ async function typing(text) {
     translatedTextDiv.innerHTML = `<p>${textToDisplay}</p>`;
     translatorRowDiv.appendChild(translatedTextDiv);
   } catch (error) {
-    // Handle the error if needed
     console.error("Error in typing:", error);
   }
+}
+
+function changeFromLang(opt) {
+  console.log(opt);
+}
+
+function changeToLang(opt) {
+  console.log(opt);
 }
