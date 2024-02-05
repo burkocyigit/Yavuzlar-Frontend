f52a0eac9515f2bc382ee5b68877d4e653477a58
Burak <brkkcygt32@gmail.com>
2024-02-05 22:43:48 +0300

    Create & delete blogs && 404 page

--- Week05/netninja-tutorial/my-blog/src/App.js
+++ Week05/netninja-tutorial/my-blog/src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Create from "./Create";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/create" element={<Create />} />
               <Route path="/blogs/:id" element={<BlogDetails />} />
+              <Route path="*" element={<NotFound />} />
+              <Route path="/blogs/*" element={<NotFound />} />
             </Routes>
           </div>
         </div>
